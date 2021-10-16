2795ba9e3f258bbb0bdffded30f12718a576d492
Tapash Mollick <tapashmollick@Tapashs-MacBook-Pro.local>
2021-10-16 08:49:43 +0530

    Extended the copy on write with the example of an array passing to multiple functions

--- RevengePlayground.playground/Contents.swift
+++ RevengePlayground.playground/Contents.swift
@@ -1,10 +1,5 @@
 import UIKit
 
-///Copy on write
-
-let array = [1, 2, 3, 4, 5]
-var array2 = array
-
 struct MemoryAddress<T>: CustomStringConvertible {
     let intValue: Int
     var description: String {
@@ -17,6 +12,13 @@ struct MemoryAddress<T>: CustomStringConvertible {
     }
 }
 
+//**********************************Copy on write
+
+let array = [1, 2, 3, 4, 5]
+var array2 = array
+
+
+
 print(MemoryAddress(of: array))
 print(MemoryAddress(of: array2))
 ///  Till now both the addresses are same.
@@ -25,3 +27,47 @@ array2.append(6) /// Copy on write now both the addresses are different. as the 
 print(MemoryAddress(of: array))
 print(MemoryAddress(of: array2))
 /// Now both the addresses are  different.
+
+
+//*******************************************************************************//
+/// Passing the array as an argument to multiple methods
+var array3 = [6, 2, 3, 4, 5]
+
+func update(_ array: inout [Int]) {
+    print("\(MemoryAddress(of: array)) for \(#function)")
+}
+
+func update2(_ array: inout [Int]) {
+    print("\(MemoryAddress(of: array)) for \(#function)")
+}
+
+func update3(_ array: inout [Int]) {
+    print("\(MemoryAddress(of: array)) for \(#function)")
+}
+
+/// Before updating the array content
+debugPrint("******************Before updating the array content****************")
+print(MemoryAddress(of: array3))
+update(&array3)
+update2(&array3)
+update3(&array3)
+
+
+/// After updating the array content
+debugPrint("******************After updating the array content****************")
+print(MemoryAddress(of: array3))
+update(&array3)
+update2(&array3)
+update3(&array3)
+func update4(_ array: inout [Int]) {
+    array.append(9)
+    print("\(MemoryAddress(of: array)) for \(#function)")
+}
+update4(&array3)
+
+
+
+
+
+
+
